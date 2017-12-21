67ac4a9fbdf3d44245ca438ff1731b8aef2b1e5d
Alex <kik9399@mail.ru>
2017-12-21 21:28:56 +0300

    unicode crypt-decrypt test

---
+++ tests/source/init.cpp
@@ -0,0 +1,27 @@
+#include <rsa.hpp>
+#include <catch.hpp>
+
+SCENARIO("check every uniCODE")
+{
+	vector<long> char_codes;
+	for (unsigned int i = 0; i < 109384; ++i)
+	{
+		char_codes.push_back(i);
+	}
+	
+	Rsa rsa;
+	Key keys = rsa.produce_keys();
+
+	vector<long> crypted_codes;
+	for (unsigned int i = 0; i < char_codes.size(); ++i)
+	{
+		crypted_codes.push_back(rsa.endecrypt(char_codes.at(i),keys.ekey,keys.pkey));
+	}
+	
+	for (auto &it: crypted_codes)
+	{
+		it = rsa.endecrypt(it,keys.dkey,keys.pkey);
+	}
+	
+	REQUIRE (char_codes == crypted_codes)
+}
