d66412582bbd74d996cabaa8aacb23256d993d0f
Alex <kik9399@mail.ru>
2017-12-21 21:48:31 +0300

    Fixed test, added status icon

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # RSA_MODULE
 
 Тестирование модуля RSA для мобильного чата 
+
+Travis status:
+[![Build Status](https://travis-ci.org/akosoj/RSA_MODULE.svg?branch=master)](https://travis-ci.org/akosoj/RSA_MODULE)
--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -3,7 +3,7 @@
 
 SCENARIO("check every uniCODE")
 {
-	vector<long> char_codes;
+	std::vector<long> char_codes;
 	for (unsigned int i = 0; i < 109384; ++i)
 	{
 		char_codes.push_back(i);
@@ -12,7 +12,7 @@ SCENARIO("check every uniCODE")
 	Rsa rsa;
 	Key keys = rsa.produce_keys();
 
-	vector<long> crypted_codes;
+	std::vector<long> crypted_codes;
 	for (unsigned int i = 0; i < char_codes.size(); ++i)
 	{
 		crypted_codes.push_back(rsa.endecrypt(char_codes.at(i),keys.ekey,keys.pkey));
@@ -23,5 +23,5 @@ SCENARIO("check every uniCODE")
 		it = rsa.endecrypt(it,keys.dkey,keys.pkey);
 	}
 	
-	REQUIRE (char_codes == crypted_codes)
+	REQUIRE (char_codes == crypted_codes);
 }
